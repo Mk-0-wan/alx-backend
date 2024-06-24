922947a499c987efe301d36087c5f04458bfe0ca
gh057 <victormurithi2020@gmail.com>
2024-06-25 00:37:37 +0300

    start is less than one page less

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,13 +7,8 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Function that returns the index range from the starting idx to
     the ending idx"""
-    y = 0
-    x = 0
     if ((page - 1) == 0):
-        y = page_size
-        y *= page
-    else:
-        y = page_size
-        x = (page_size * (page - 1))
-        y += (page_size * (page - 1))
-    return (x, y)
+        page_size *= page
+        return ((page - 1), page_size)
+
+    return (page_size * (page - 1), page_size * page)
