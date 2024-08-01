126f98f13e530ca56999a9e86f94e5f2621c98cf
gh057 <victormurithi2020@gmail.com>
2024-08-01 19:06:08 +0300

    updated the job creation with some values

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -8,8 +8,8 @@ import { createQueue } from 'kue'
 const queue = createQueue();
 
 const job = queue.create('push_notification_code', {
-  phoneNumber: '',
-  message: '',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 }).save((err) => {
   if (!err) {
     console.log(`Notification job created: ${job.id}`);
